8da290649e0b1013a6fd943c98be8b5f37cceb16
maprezdev <perezdhoz@gmail.com>
2019-11-07 10:46:18 -0500

    Update djrestful/settings.py file

--- djrestful/settings.py
+++ djrestful/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Django REST framework
+    'rest_framework',
+    # Toys application
+    'toys.apps.ToysConfig'
 ]
 
 MIDDLEWARE = [
